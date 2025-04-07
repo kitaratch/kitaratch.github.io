8a0d63523d0e15166b33eb6276827888d11f1793
Koichi Ito <kito@j-code.org>
2025-04-07 20:29:45 +0900

    デプロイの設定

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,3 @@
-# kitaratch-website
-
-
-# ビルドの方法
-docker-compose build
-docker-compose up -d
-docker exec -it kitaratch-website /bin/bash
-
-# ビルドの手順
-
-script/install.sh を参照の事
-
 # scratch-gui のインストール
 cd /
 git clone --branch v3.6.18 --single-branch https://github.com/LLK/scratch-gui.git
@@ -21,23 +9,10 @@ cd /scratch-gui
 git clone -b stretch3 --single-branch https://github.com/microbit-more/mbit-more-v2.git ./microbitMore
 sh ./microbitMore/install-stretch3.sh
 
-# pcratch IoT 拡張のインストール（ビルドはダメヨよ）
+# pcratch IoT 拡張のインストール（この環境ではビルドは出来ない）
 cd /scratch-gui
 git clone https://github.com/jcodeorg/pcratch-iot-ext.git
-# cd pcratch-iot-ext
-# npm install
-# npm run build
-# cd /scratch-gui
 sh ./pcratch-iot-ext/install-stretch3.sh
-# 確認
-# cp ./node_modules/scratch-vm/src/extension-support/extension-manager.js /scripts
-
-# pcratch IoT 拡張のレポジトリ同期
-cd /scratch-gui/pcratch-iot-ext
-git fetch
-git pull
-cp dist/pcratchIoT.mjs /scratch-gui/node_modules/scratch-vm/src/extensions/pcratchIoT/
-ls /scratch-gui/node_modules/scratch-vm/src/extensions/pcratchIoT/
 
 # scratch-gui のビルド
 cd /scratch-gui
